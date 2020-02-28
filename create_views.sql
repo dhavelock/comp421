b14fe013e8b3b4c806906b7130d0d6ff2c83c306
Sam Cattani <samantha.cattani@mail.mcgill.ca>
2020-02-28 17:19:17 -0500

    Update create_views.sql

--- create_views.sql
+++ create_views.sql
@@ -1,6 +1,10 @@
+-- create a view to see all information about all menu items --
+
 CREATE OR REPLACE VIEW all_menu_info AS
-  SELECT menuitems.item_name, description, price, menu_name
-  FROM menuitems
-  LEFT JOIN menutomenuitems
-  ON menuitems.item_name = menutomenuitems.item_name
-  ORDER BY price DESC;
+SELECT menuitems.item_name, description, price, menu_name
+FROM menuitems
+LEFT JOIN menutomenuitems
+ON menuitems.item_name = menutomenuitems.item_name
+ORDER BY price DESC;
+
+-- create a view to see all payments and the order that they correspond to --
