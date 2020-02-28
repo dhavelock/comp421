efd69f5d1e3e378794e8881df3580645a31a4f08
connorplante <35504431+connorplante@users.noreply.github.com>
2020-02-28 16:37:51 -0500

    Create illegal_modifications.sql

---
+++ illegal_modifications.sql
@@ -0,0 +1,15 @@
+-- Attempt to make employees wage negative
+UPDATE Employees
+SET wage = -1.00
+WHERE Employees.eid = 4;
+
+-- Add Table #20 to Section 1 with 0 seats
+INSERT INTO Tables VALUES (20, 0, 1);
+
+-- Insert a payment that has a negative amount value
+INSERT INTO Payments (bid, payment_date, payment_time, payment_type, amount, is_successful) VALUES (1, '2020-02-06', '19:34', 'visa', -177.10, TRUE);
+
+-- Attempt to update the price of Ravioli to a negative value
+UPDATE MenuItems
+SET price = -17.99
+WHERE MenuItems.item_name = 'Carrot and Garam Masala Ravioli';
