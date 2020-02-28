07c3ac2d4e16dd96d490e4c44d902e05665276a8
Sam Cattani <samantha.cattani@mail.mcgill.ca>
2020-02-28 17:18:06 -0500

    Create create_views.sql

---
+++ create_views.sql
@@ -0,0 +1,6 @@
+CREATE OR REPLACE VIEW all_menu_info AS
+  SELECT menuitems.item_name, description, price, menu_name
+  FROM menuitems
+  LEFT JOIN menutomenuitems
+  ON menuitems.item_name = menutomenuitems.item_name
+  ORDER BY price DESC;
