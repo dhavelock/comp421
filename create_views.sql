ee7bc29a7346298b3028f3acd0e47532aac65681
Sam Cattani <samantha.cattani@mail.mcgill.ca>
2020-02-28 17:52:34 -0500

    Update create_views.sql

--- create_views.sql
+++ create_views.sql
@@ -7,4 +7,12 @@ LEFT JOIN menutomenuitems
 ON menuitems.item_name = menutomenuitems.item_name
 ORDER BY price DESC;
 
--- create a view to see all payments and the order that they correspond to --
+-- create a view to see most popular menu items and their price and total sales --
+CREATE OR REPLACE VIEW most_popular_items AS
+SELECT OrderMenuItems.item_name, MenuItems.price, SUM(OrderMenuItems.quantity) AS quantity, SUM(OrderMenuItems.quantity * MenuItems.price) AS total
+FROM ordermenuitems 
+LEFT JOIN menuitems
+ON ordermenuitems.item_name = menuitems.item_name
+LEFT JOIN menutomenuitems
+ON ordermenuitems.item_name = menutomenuitems.item_name
+GROUP BY ordermenuitems.item_name, menuitems.price ORDER BY quantity DESC;
