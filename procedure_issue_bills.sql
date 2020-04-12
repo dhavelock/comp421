182b39cb83c9a4b32f61fd03c58dd6003c74017b
Dylan Havelock <dylan.havelock@gmail.com>
2020-04-12 19:21:35 -0400

    creativity

---
+++ procedure_issue_bills.sql
@@ -0,0 +1,29 @@
+CREATE OR REPLACE PROCEDURE issue_all_outstanding_bills()
+   AS $$
+
+DECLARE 
+	rec_order RECORD;
+
+	cursor_outstanding_order CURSOR FOR SELECT Orders.oid FROM Orders WHERE Orders.oid NOT IN (SELECT Bills.oid FROM Bills);
+BEGIN
+    OPEN cursor_outstanding_order;
+	
+    LOOP
+        FETCH cursor_outstanding_order INTO rec_order;
+        EXIT WHEN NOT FOUND;
+
+        INSERT INTO Bills (oid, total) 
+        SELECT oid, sum ( MenuItems.price * OrderMenuItems.quantity) as item_total
+        FROM OrderMenuItems INNER JOIN MenuItems
+        ON OrderMenuItems.item_name = MenuItems.item_name
+        GROUP BY OrderMenuItems.oid ORDER BY OrderMenuItems.oid
+  
+  END LOOP;
+
+CLOSE cursor_outstanding_order;
+
+END; 
+
+$$
+
+LANGUAGE plpgsql;
