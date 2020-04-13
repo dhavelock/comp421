c5f66521ea1854f9fd33c1349fe61506eb3a0712
Sam Cattani <samantha.cattani@mail.mcgill.ca>
2020-04-12 22:37:58 -0400

    Update procedure_issue_bills.sql

--- procedure_issue_bills.sql
+++ procedure_issue_bills.sql
@@ -2,25 +2,33 @@ CREATE OR REPLACE PROCEDURE issue_all_outstanding_bills()
    AS $$
 
 DECLARE 
-	rec_order RECORD;
 
+	rec_order RECORD;
+	amount_owed NUMERIC(10,2);
 	cursor_outstanding_order CURSOR FOR SELECT Orders.oid FROM Orders WHERE Orders.oid NOT IN (SELECT Bills.oid FROM Bills);
+
 BEGIN
+
     OPEN cursor_outstanding_order;
-	
+
     LOOP
+
         FETCH cursor_outstanding_order INTO rec_order;
         EXIT WHEN NOT FOUND;
 
-        INSERT INTO Bills (oid, total) 
-        SELECT oid, sum ( MenuItems.price * OrderMenuItems.quantity) as item_total
-        FROM OrderMenuItems INNER JOIN MenuItems
-        ON OrderMenuItems.item_name = MenuItems.item_name AND OrderMenuItems.oid = rec_order.oid
-        GROUP BY OrderMenuItems.oid ORDER BY OrderMenuItems.oid
-  
-  END LOOP;
 
-CLOSE cursor_outstanding_order;
+	SELECT sum ( MenuItems.price * OrderMenuItems.quantity) as item_total INTO amount_owed 
+		FROM OrderMenuItems INNER JOIN MenuItems
+		ON OrderMenuItems.item_name = MenuItems.item_name 
+		AND OrderMenuItems.oid = rec_order.oid
+		GROUP BY OrderMenuItems.oid ORDER BY OrderMenuItems.oid;
+	
+	INSERT INTO Bills(oid, total) VALUES (rec_order.oid, amount_owed);
+  
+
+     END LOOP;
+
+     CLOSE cursor_outstanding_order;
 
 END; 
 
